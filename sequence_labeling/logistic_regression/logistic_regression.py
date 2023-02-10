5e1b4047c0b7c1a429b981d392c6cbba3ebe616f
Sahar <saharzal78@gmail.com>
2023-02-10 17:57:52 +0330

    logistic regression saved model added

--- sequence_labeling/logistic_regression/logistic_regression.py
+++ sequence_labeling/logistic_regression/logistic_regression.py
@@ -1,21 +1,25 @@
 import numpy as np
 import pandas as pd
-import math
+import pickle
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.model_selection import train_test_split #used for splitting data into train and test
 from sklearn import metrics  #for scoring performance
-from sklearn.model_selection import cross_val_score, StratifiedKFold
-from statistics import mean
+from sklearn.model_selection import cross_val_score, StratifiedKFold,KFold,cross_validate
+from statistics import mean,variance,stdev
 
 class LogisticRegTokenClassification:
-    def __init__(self):
-        self.tagged_sentences = []
-        self.__load_data()
-        self.vectorizer = TfidfVectorizer(stop_words=None, ngram_range=(1, 3),max_features=200000)
-        self.logisticRegression = LogisticRegression(C=5e1, solver='lbfgs', multi_class='multinomial', random_state=41,n_jobs=-1)
-        self.skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=41)
+    def __init__(self,splits=25,ngram=3):
+        self.__load_model()
+
+        #### TRAINING PARAMETERS ####
+        # self.tagged_sentences = []
+        # self.__load_data()
+        # self.vectorizer = TfidfVectorizer(ngram_range=(1, ngram),max_features=10000)
+        # self.logisticRegression = LogisticRegression(C=5e1, solver='lbfgs', random_state=0,max_iter=500)
+        # self.skf = StratifiedKFold(n_splits=splits, shuffle=True, random_state=25)
+        # self.skf = KFold(n_splits=splits, shuffle=True, random_state=0)
 
     def __load_data(self):
         df1 = pd.read_csv("../data/Data - Sheet1.csv")
@@ -30,7 +34,12 @@ class LogisticRegTokenClassification:
                 tagged_sentence = []
             else:
                 tagged_sentence.append((token[0], token[1]))
-        # print(self.tagged_sentences[0])
+
+    def __load_model(self):
+        filename = "./log_reg_token_cls_model.pickle" # Modal Path
+        loaded_model = pickle.load(open(filename, 'rb'))
+        self.logisticRegression = loaded_model["model"]
+        self.vectorizer = loaded_model["vectorizer"]
 
     def get_sentence_from_array(self,sent_array):
         return " ".join(sent_array)
@@ -46,11 +55,11 @@ class LogisticRegTokenClassification:
                 vector = []
                 for i in range(j, j + (2 * window_size) + 1):
                     if i < 0:
-                        vector.append("<pad>")
+                        vector.append("سلام")
                     elif i >= 0 and i < sent_len:
                         vector.append(sent[i][0])
                     else:
-                        vector.append("<pad>")
+                        vector.append("سلام")
                 sent_vectors.append(self.get_sentence_from_array(vector))
                 if save_labels:
                     sent_vectors_lables.append(sent[j + window_size][1])
@@ -67,14 +76,32 @@ class LogisticRegTokenClassification:
         return self.vectorizer.transform(sequences)
 
 
-    def get_cross_validation_scores(self, sequences, labels, scoring_metric='f1_macro'):
-        return cross_val_score(self.logisticRegression, sequences, labels, cv=self.skf, scoring=scoring_metric)
+    def get_cross_validation_scores(self, sequences, labels, scoring_metric='accuracy'):
+        return cross_validate(self.logisticRegression, sequences, labels, cv=self.skf, scoring=['accuracy','precision','recall','f1_micro','f1_macro'])
+        
 
     def train_by_cross_validation(self, window_size):
         sequences, labels = self.get_sentences_token_sequences_and_labels(self.tagged_sentences, window_size)
         all_vectors = self.vectorizer.fit_transform(sequences)
-        csv = self.get_cross_validation_scores(all_vectors, labels)
-        return csv
+        # csv = self.get_cross_validation_scores(all_vectors, labels)
+        # return csv
+        scores = []
+        labels = np.array(labels)
+        for train_index, test_index in self.skf.split(all_vectors):
+            X_train, X_test = all_vectors[train_index], all_vectors[test_index]
+            y_train, y_test = labels[train_index], labels[test_index]
+            self.logisticRegression.fit(X_train, y_train)
+            score = self.logisticRegression.score(X_test, y_test)
+            # score = clf.score(X_test, y_test)
+            y_predict = self.logisticRegression.predict(X_test)
+            score = {"accuracy":0,"recall":0,"percision":0,"f1_macro":0}
+            score["accuracy"] = metrics.accuracy_score(y_test,y_predict)
+            # score["recall"] = metrics.recall_score(y_test,y_predict,average="macro")
+            # score["percision"] = metrics.precision_score(y_test,y_predict,average="macro")
+            # score["f1_macro"] = metrics.f1_score(y_test,y_predict,average="macro")
+            # score["f1_micro"] = metrics.f1_score(y_test,y_predict,average="micro")
+            scores.append(score)
+        return scores
 
     def get_sentence_windows(self, sentence, window_size):
         sent_len = len(sentence)
@@ -85,31 +112,65 @@ class LogisticRegTokenClassification:
             vector = []
             for i in range(j, j + (2 * window_size) + 1):
                 if i < 0:
-                    vector.append("<pad>")
+                    vector.append("<پد>")
                 elif i >= 0 and i < sent_len:
                     vector.append(sentence[i])
                 else:
-                    vector.append("<pad>")
+                    vector.append("<پد>")
             sent_vectors.append(self.get_sentence_from_array(vector))
         return sent_vectors
 
-    def predict_sentence(self, sentence):
-        sentence_sequences = self.get_sentence_windows(sentence.split(" "), 1)
-        sequnce_vectors = self.transform_tfidf_vectors(sentence_sequences)
-        y_predicts = self.logisticRegression.predict(sequnce_vectors)
-        return y_predicts
-
-    def train_model(self):
+    def train_test_model(self):
         windows, labels = self.get_sentences_token_sequences_and_labels(self.tagged_sentences, 1)
         x_train, x_test, y_train, y_test = self.get_train_test_data(windows, labels)
         self.fit_tfidf_vectors(x_train)
         self.logisticRegression.fit(self.train_vectors, y_train)
+        x_test_vectos = self.transform_tfidf_vectors(x_test)
+        y_predict = self.logisticRegression.predict(x_test_vectos)
+        return metrics.f1_score(y_test, y_predict,average='macro')
 
+    def predict_sentence(self, sentence):
+        sentence_sequences = self.get_sentence_windows(sentence.split(" "), 1)
+        sequnce_vectors = self.transform_tfidf_vectors(sentence_sequences)
+        y_predict = self.logisticRegression.predict(sequnce_vectors)
+        return y_predict
 
 if __name__ == "__main__":
     logisticRegression = LogisticRegTokenClassification()
-    # windows, labels = logisticRegression.get_sentences_windows_labels(logisticRegression.tagged_sentences,0)
-    # mean_csv = mean(logisticRegression.train_by_cross_validation(1))
+
+    sentence = input()
+    print(logisticRegression.predict_sentence(sentence))
+
+    ##### TRAINING MODEL #####
+    # csv = logisticRegression.train_by_cross_validation(1)
+
+    ##### PRINT SCORES #####
+    # print(csv)
+    # print("mean accuracy: ",mean([score["accuracy"] for score in csv]))
+    # print("stdev accuracy: ",stdev([score["accuracy"] for score in csv]))
+    
+    # print("mean recall: ",mean([score["recall"] for score in csv]))
+    # print("stdev recall: ",stdev([score["recall"] for score in csv]))
+
+    # print("mean percision: ",mean([score["percision"] for score in csv]))
+    # print("stdev percision: ",stdev([score["percision"] for score in csv]))
+
+    # print("mean f1_micro: ",mean([score["f1_micro"] for score in csv]))
+    # print("stdev f1_micro: ",stdev([score["f1_micro"] for score in csv]))
+
+    # print("mean f1_macro: ",mean([score["f1_macro"] for score in csv]))
+    # print("stdev f1_macro: ",stdev([score["f1_macro"] for score in csv]))
+    
     # print("mean: ", mean_csv)
-    logisticRegression.train_model()
-    print(logisticRegression.predict_sentence("سلام باشه اومدم"))
+    # print("variance: ", variance_csv)
+    # print("stdev: ", stdev_csv)
+    # print("f1: ", logisticRegression.train_test_model())
+
+
+    ##### save the model to disk #####
+    # filename = 'log_reg_token_cls_model.pickle'
+    # pickle.dump({"model":logisticRegression.logisticRegression,"vectorizer":logisticRegression.vectorizer}, open(filename, 'wb'))
+
+
+    ##### PREDICT #####
+    # print(logisticRegression.predict_sentence("فک کنم اومدی اون روز"))
